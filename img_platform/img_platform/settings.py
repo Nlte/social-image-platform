cc17aa0ff3ad40e36db635b79d59a97e88cd1515
Nlte <nathanael.leaute@insa-lyon.fr>
2016-11-23 23:24:42 +0100

    django settings
    swapped back to Debug = true

--- img_platform/img_platform/settings.py
+++ img_platform/img_platform/settings.py
@@ -26,10 +26,7 @@ sys.path.append(os.path.join(PROJECT_ROOT, "ml_core"))
 SECRET_KEY = 'k(c!scqmzrxoszw%--d$(r(5n%une9j#^nql&=l^c3bypbq0$_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
-
+DEBUG = True
 
 # Application definition
 
